a198875665ec54f4a6eca903d412cb7ce811ffd2
Kiattikhun <kiattikhun.wantamard@gmail.com>
2023-07-06 16:58:50 +0700

    delete comments

--- test/specs/sf-ticket.e2e.ts
+++ test/specs/sf-ticket.e2e.ts
@@ -64,22 +64,3 @@ describe("SF CINEMA", () => {
     });
   });
 });
-// it("Can select movie", async () => {
-//   const movieName = "Mission: Impossible - Dead Reckoning Part One";
-//   await LandingPage.selectMovie(movieName);
-//   await expect(browser).toHaveUrlContaining("showtime");
-// });
-// describe("Showtime Page", () => {
-//   it("Can search cinema by location and select a highlight time", async () => {
-//     const cinemaLocation = "Buriram";
-//     await ShowTime.searchMovieBar.setValue(cinemaLocation);
-//     await ShowTime.showTimeActiveBtn.click();
-//     await expect(browser).toHaveUrlContaining("select-seat");
-//   });
-// });
-// describe("Select Seat Page", () => {
-//   it("Can select a seat", async () => {
-//     const seats = ["H1", "H2"];
-//     await SelectSeat.selectSeat(seats);
-//   });
-// });
