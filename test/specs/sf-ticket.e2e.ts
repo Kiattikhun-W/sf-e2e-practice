d33f5fa7a0f7698961f9089551f6d9d70c1ee9ee
Kiattikhun <kiattikhun.wantamard@gmail.com>
2023-07-06 16:50:54 +0700

    add reserve testcase

--- test/specs/sf-ticket.e2e.ts
+++ test/specs/sf-ticket.e2e.ts
@@ -26,25 +26,51 @@ describe("SF CINEMA", () => {
     it("Can open movie menu", async function () {
       await LandingPage.openMovieMenu();
     });
+  });
+  describe("Resever seat functionality", () => {
+    /* 
+      Prequsite:
+      - Landing Page is opened
+      - Policy Modal is closed
+      - Cover Page is closed if it's existing
+      - Langaue need to changed to English
+    
+    */
+    beforeEach(async function () {
+      await browser.reloadSession();
+      await browser.maximizeWindow();
+      await LandingPage.open();
+      await LandingPage.preparePageStartUp();
+      await LandingPage.changeLanguage();
+    });
 
-    // it("Can select movie", async () => {
-    //   const movieName = "Mission: Impossible - Dead Reckoning Part One";
-    //   await LandingPage.selectMovie(movieName);
-    //   await expect(browser).toHaveUrlContaining("showtime");
-    // });
-    // describe("Showtime Page", () => {
-    //   it("Can search cinema by location and select a highlight time", async () => {
-    //     const cinemaLocation = "Buriram";
-    //     await ShowTime.searchMovieBar.setValue(cinemaLocation);
-    //     await ShowTime.showTimeActiveBtn.click();
-    //     await expect(browser).toHaveUrlContaining("select-seat");
-    //   });
-    // });
-    // describe("Select Seat Page", () => {
-    //   it("Can select a seat", async () => {
-    //     const seats = ["H1", "H2"];
-    //     await SelectSeat.selectSeat(seats);
-    //   });
-    // });
+    afterEach(async () => {
+      await browser.closeWindow();
+    });
+
+    it("Can reserve seat from the landing page", async () => {
+      const movieName = "Mission: Impossible - Dead Reckoning Part One";
+      await LandingPage.selectMovie(movieName);
+      await expect(browser).toHaveUrlContaining("showtime");
+    });
   });
 });
+// it("Can select movie", async () => {
+//   const movieName = "Mission: Impossible - Dead Reckoning Part One";
+//   await LandingPage.selectMovie(movieName);
+//   await expect(browser).toHaveUrlContaining("showtime");
+// });
+// describe("Showtime Page", () => {
+//   it("Can search cinema by location and select a highlight time", async () => {
+//     const cinemaLocation = "Buriram";
+//     await ShowTime.searchMovieBar.setValue(cinemaLocation);
+//     await ShowTime.showTimeActiveBtn.click();
+//     await expect(browser).toHaveUrlContaining("select-seat");
+//   });
+// });
+// describe("Select Seat Page", () => {
+//   it("Can select a seat", async () => {
+//     const seats = ["H1", "H2"];
+//     await SelectSeat.selectSeat(seats);
+//   });
+// });
